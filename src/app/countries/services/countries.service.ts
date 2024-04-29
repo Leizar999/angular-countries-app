122c3c482c3bc84f6defe66b8ef5eb1974048d6e
Juan Carlos <juan.carlos@insparx.com>
2024-04-29 22:34:08 +0200

    Added info and country details and translations

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -10,6 +10,17 @@ export class CountriesService {
 
 
   constructor(private http: HttpClient) { }
+
+  public searchCountryByAlphaCode( code: string ): Observable<Country | null> {
+
+    const url = `${ this.apiUrl }/alpha/${ code }`;
+
+    return this.http.get<Country[]>( url )
+    .pipe(
+      map( countries => countries.length > 0 ? countries[0] : null),
+      catchError( error => of(null))
+    );
+  }
 
   public searchCapital( term: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ term }`;
